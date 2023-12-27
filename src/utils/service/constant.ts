f50ab3a04e2a8515b272ac628ef1fa9b0b2809a7
frckbrice <maebrie2017@gmail.com>
2023-12-27 21:11:32 +0100

    test intergration

--- src/utils/service/constant.ts
+++ src/utils/service/constant.ts
@@ -2,7 +2,7 @@ import io from "socket.io-client";
 export const GOOGLE_CLIENT = process.env.NEXT_PUBLIC_GOOGLE_CLIENT;
 
 export const api_call = process.env.NEXT_PUBLIC_LOCAL_BURL;
-export const public_call = process.env.NEXT_PUBLIC_LOCAL_FURL;
+export const public_call = process.env.NEXT_PUBLIC_VERCEL_URL;
 export const socket = io(api_call || "", {
   path: "/socket.io/",
   reconnectionDelay: 1000,
